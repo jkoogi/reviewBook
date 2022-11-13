2463e3832215b1e306364b8d4f9b11d75e45b606
jkoog <jkoogi@naver.com>
2022-11-13 19:25:08 +0900

    * lombok lib 추가

--- 스프링 부트 쇼핑몰 프로젝트 with JPA/demo/src/main/java/com/example/demo/DemoApplication.java
+++ 스프링 부트 쇼핑몰 프로젝트 with JPA/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +13,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping(value = "/")
+	public String HelloWorld() {
+		return "Hello World";
+	}
 }
