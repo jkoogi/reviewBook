be6a426e042466a6587f3938479d54f433edb141
jkoog <jkoogi@naver.com>
2022-12-01 20:12:12 +0900

    * [함께 해봐요 2-5] OR 조건 처리하기  - 인터페이스에 조회메서드 추가  - 조회메서드 테스트 추가

--- 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootTest
-@TestPropertySource(locations="classpath:application-test.properties")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
 
     @Autowired
@@ -20,7 +20,7 @@ class ItemRepositoryTest {
 
     @Test
     @DisplayName("상품 저장 테스트")
-    public void createItemTest(){
+    public void createItemTest() {
         Item item = new Item();
         item.setItemNm("테스트 상풍");
         item.setPrice(10000);
@@ -34,7 +34,7 @@ class ItemRepositoryTest {
     }
 
     public void createItemList() {
-        for (int i=1; i<=10; i++) {
+        for (int i = 1; i <= 10; i++) {
             Item item = new Item();
             item.setItemNm("테스트 상품" + i);
             item.setPrice(10000 + i);
@@ -49,10 +49,20 @@ class ItemRepositoryTest {
 
     @Test
     @DisplayName("상품명 조회 테스트")
-    public void findByItemNmTest(){
+    public void findByItemNmTest() {
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
-        for(Item item : itemList) {
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
