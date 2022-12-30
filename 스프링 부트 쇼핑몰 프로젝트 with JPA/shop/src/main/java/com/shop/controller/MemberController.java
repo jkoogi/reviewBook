74b4bf5a43c273ec91eba697a56942b9ca16e14e
jkoog <jkoogi@naver.com>
2022-12-30 10:54:34 +0900

    * [함께 해봐요 4-5] 회원 가입 페이지 작성하기  - #3 memberForm.html 이슈대응 : _csrf.token

--- 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/controller/MemberController.java
+++ 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
-public class Membercontroller {
+public class MemberController {
 
     private final MemberService memberService;
 
