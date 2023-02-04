ffa8ea181006cb2539aa049936808358a44d1dc9
jkoog <jkoogi@naver.com>
2023-02-04 21:22:18 +0900

    * [함께 해봐요 4-11] 페이지 권한 설정하기  - $1 itemForm.html : 상품등록 임시 페이지 추가  - $2 ItemController.java : 상품등록 화면접근 처리  - $3 CustomAuthenticationEntryPoint.java : 요청거래 권한처리 기능 추가  - $4 SecurityConfig.java : 리소스 경로 권한처리 설정  - $5 Member.java : 관리자 테스트 코드 수정

--- 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/config/SecurityConfig.java
+++ 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,7 +22,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
         http.formLogin()
                 .loginPage("/members/login")
                 .defaultSuccessUrl("/")
@@ -31,8 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
                 .logoutSuccessUrl("/")
-        ;
+                ;
 
+        http.authorizeRequests()
+                .mvcMatchers("/", "/members/**",
+                        "/item/**", "/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                ;
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                ;
     }
 
     @Bean
@@ -44,5 +54,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth)throws Exception{
         auth.userDetailsService(memberService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 }
