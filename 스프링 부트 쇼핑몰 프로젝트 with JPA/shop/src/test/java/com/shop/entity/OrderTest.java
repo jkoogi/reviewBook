174b36bd27260f4a6e37b5c0b6259f9de4ee7511
jkoog <jkoogi@naver.com>
2023-02-09 23:49:52 +0900

    * [함께 해봐요 5-7] 주문 엔티티 조회 테스트하기(즉시 로딩) #2  - $1 OrderItem.java : 지연로딩 패치전략 속성 설정  - $2 OrderTest.java : 지연 로딩 테스트 수정

--- 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/test/java/com/shop/entity/OrderTest.java
+++ 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/test/java/com/shop/entity/OrderTest.java
@@ -121,8 +121,10 @@ public class OrderTest {
 
         OrderItem orderItem = orderItemRepository.findById(orderItemId)
                 .orElseThrow(EntityNotFoundException::new);
-        System.out.println("Order class : " +
-                orderItem.getOrder().getClass());
+        System.out.println("Order class : "+orderItem.getOrder().getClass());
+        System.out.println("=========================");
+        orderItem.getOrder().getOrderDate();
+        System.out.println("=========================");
 
     }
 
