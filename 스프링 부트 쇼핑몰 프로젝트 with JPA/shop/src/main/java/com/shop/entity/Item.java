c09cae87ce7c89e221947632c6796f8c77f32c2b
jkoogi <jkoogi@naver.com>
2023-02-10 13:36:14 +0900

    * [함께 해봐요 5-9] Auditing 기능을 활용한 데이터 추적하기  - $01 AuditorAwarelmpl.java : 로그인한 사용자의 정보를 등록자와 수정자로 지정 추가  - $02 AuditConfig.java : Auditing 기능사용 설정 추가  - $03 BaseTimeEntity.java : 시간만 관리하는 유형 추가  - $04 BaseEntity.java : 일자, 사용자 모두 관리 추가  - $05 Member.java : BaseEntity 상속  - $06 MemgerTest.java : Auditing 테스트 추가  - $07 Cart.java : BaseEntity 상속  - $08 CartItem.java : BaseEntity 상속  - $09 Item.java : BaseEntity 상속, 시간 삭제  - $10 Order.java : BaseEntity 상속, 시간 삭제  - $11 OrderItem.java : BaseEntity 상속, 시간 삭제

--- 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/entity/Item.java
+++ 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/entity/Item.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity{
 
     @Id
     @Column(name="item_id")
@@ -37,9 +37,9 @@ public class Item {
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; //상품 판매상태
 
-    private LocalDateTime regTime; //등록 시간
+//    private LocalDateTime regTime; //등록 시간
 
-    private LocalDateTime updateTime; //수정 시간
+//    private LocalDateTime updateTime; //수정 시간
 
 //    @ManyToMany
 //    @JoinTable(
--- 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/entity/Order.java
+++ 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/entity/Order.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter @Setter
-public class Order {
+public class Order extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "order_id")
@@ -31,8 +31,8 @@ public class Order {
             , orphanRemoval = true, fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    private LocalDateTime regTime;
+//    private LocalDateTime regTime;
 
-    private LocalDateTime updateTime;
+//    private LocalDateTime updateTime;
 
 }
