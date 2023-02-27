c55b270d0fae3f39f1d4e0357d429de3d93d06c1
jkoog <jkoogi@naver.com>
2023-02-27 23:24:56 +0900

    *［함께 해봐요 8네 장바구니 담기 구현하기  - $01 CartItemDto.java : 상품 상세 페이지 정보전달  - $02 Cart.java : 카드 엔티티 추가  - $03 CartItem.java : 카트상품 엔티티 추가  - $04 CartRepository.java :Cart 엔티티 조회 메서드 추가  - $05 CartItemRepository.java :  인터페이스를 생성  - $06 CartService.java : 카트서비스 추가  - $07 CartController.java : 카트서비스 호출

---
+++ 스프링 부트 쇼핑몰 프로젝트 with JPA/shop/src/main/java/com/shop/repository/CartService.java
@@ -0,0 +1,46 @@
+package com.shop.repository;
+
+import com.shop.dto.CartItemDto;
+import com.shop.entity.Cart;
+import com.shop.entity.CartItem;
+import com.shop.entity.Item;
+import com.shop.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    public Long addCart(CartItemDto cartItemDto, String email){
+        Item item = itemRepository.findById(cartItemDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+        Member member = memberRepository.findByEmail(email);
+
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if (cart == null){
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+
+        CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
+
+        if(savedCartItem != null){
+            savedCartItem.addCount(cartItemDto.getCount());
+            return savedCartItem.getId();
+        } else{
+            CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
+            cartItemRepository.save(cartItem);
+            return cartItem.getId();
+        }
+    }
+}
