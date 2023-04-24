16ad2bd7ab85ec8472b977a841d97b2a70f584ac
jkoog <jkoogi@naver.com>
2023-04-24 23:45:32 +0900

    오타수정

--- 프로그래시브웹앱만들기/0201/04-2_main.js
+++ 프로그래시브웹앱만들기/0201/04-2_main.js
@@ -1,10 +1,10 @@
 
     // js 파일에서 내보낸 식별자를 가져와 conHello, fnPlusNumbers 객체에 저장
-    import {conHello, fnPlusNubers} from './04-1_ibrary_named.js';
+    import {conHello, fnPlusNubers} from './04-1_library_named.js';
     console.log("conHello : ", conHello);
     console.log("fnPlusNubers : ", fnPlusNubers(1,2));
 
     // js 파일에서 내보닌 모든 식별자를 가져와 myLibrary 객체에 저장
-    import * as myLibrary from './04-1_ibrary_named.js';
+    import * as myLibrary from './04-1_library_named.js';
     console.log("myLibrary.conHello : ", myLibrary.conHello);
     console.log("myLibrary.fnPlusNubers : ", myLibrary.fnPlusNubers(1,2));
