6f0f361671f87aa87d8e4fedba449d49324efaaa
jkoog <jkoogi@naver.com>
2023-10-03 21:00:55 +0900

    * 셈플2.1.1 * 연습문제 2.1

---
+++ 코틀린함수형프로그래밍/Kotlin/fpKotlinEx/src/main/kotlin/ex02/셈플211_코틀린의꼬리호출.kt
@@ -0,0 +1,26 @@
+package ex02
+
+import ex02.ex0201.Ex0201
+
+
+class 셈플211_코틀린의꼬리호출 {
+    // 코틀린의 꼬리 호출 예제 : n 번째 펙토리얼 구하기
+    fun factorial(i: Int): Int {
+        tailrec fun go(n: Int, acc: Int): Int = // tailrec 변경자는 꼬리 호출을 제거하라고 컴파일러에 명령
+            if (n <= 0) {		//
+//                println(" - n : "+n+" / acc : "+acc)
+                acc		//     2	1
+            }
+            else {		//
+//                println(" + n : "+n+" / acc : "+acc +" > ["+(n - 1)+", "+(n * acc)+"]")
+                // 이 함수의 마지막 재귀 호출이 꼬리 위치에 있다.
+                go(n - 1, n * acc)
+            }
+        return go(i, 1)
+    }
+}
+
+fun main() {
+    var facNum = 4
+     println("> factorial["+facNum+"] : "+셈플211_코틀린의꼬리호출().factorial(facNum))// 4*3*2*1
+}
